5f19b4445b68d66619be7106bd0a25956888dff0
Vitaliy Kosterov <kosterovvitaliy@gmail.com>
2022-07-12 20:08:20 +0300

    header

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@
 
 function App() {
 	return (
-		<div className="App">
-			FOOOD
+		<div className="wrapper">
+			<header>
+				<div className="headerLeft">
+					<img width={40} height={40} src="/public/img/logo.png" alt="" />
+					<div className="headerInfo">
+						<h3>React Sneakers</h3>
+						<p>Магазин лучших кроссовок</p>
+					</div>
+				</div>
+				<ul className="headerRight">
+					<li><img width={18} height={18} src="/public/img/cart.svg" alt="" /><span>150$</span></li>
+					<li>
+						<img width={18} height={18} src="/public/img/user.png" alt="" />
+					</li>
+				</ul>
+			</header>
+			<div className="content">
+				<h1>Все кроссовки</h1>
+				...
+			</div>
 		</div>
 	);
 }
