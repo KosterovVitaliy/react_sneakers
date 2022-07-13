e6fa918b4eb0437d2c53b32d75f80d80acc210a9
Vitaliy Kosterov <kosterovvitaliy@gmail.com>
2022-07-13 13:30:23 +0300

    header_last_part

--- src/App.js
+++ src/App.js
@@ -2,25 +2,41 @@
 
 function App() {
 	return (
-		<div className="wrapper">
-			<header>
-				<div className="headerLeft">
+		<div className="wrapper clear">
+			<header className="d-flex justify-between align-center p-40">
+				<div className="d-flex align-center">
 					<img width={40} height={40} src="/public/img/logo.png" alt="" />
-					<div className="headerInfo">
-						<h3>React Sneakers</h3>
+					<div>
+						<h3 className="text-uppercase">React Sneakers</h3>
 						<p>Магазин лучших кроссовок</p>
 					</div>
 				</div>
-				<ul className="headerRight">
-					<li><img width={18} height={18} src="/public/img/cart.svg" alt="" /><span>150$</span></li>
+				<ul className="d-flex">
+					<li className="mr-30">
+						<img width={18} height={18} src="/public/img/cart.svg" alt="" /><span>150$</span>
+					</li>
 					<li>
-						<img width={18} height={18} src="/public/img/user.png" alt="" />
+						<img width={18} height={18} src="/public/img/user.svg" alt="" />
 					</li>
 				</ul>
 			</header>
-			<div className="content">
+			<div className="content p-40">
 				<h1>Все кроссовки</h1>
-				...
+
+				<div className="card">
+					<img src="" alt="" />
+					<p></p>
+					<div>
+						<div>
+							<span>Цена:</span>
+							<b>129$</b>
+						</div>
+						<button>
+							<img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+						</button>
+					</div>
+				</div>
+
 			</div>
 		</div>
 	);
