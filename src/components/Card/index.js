380532ae01f6ad2a6eec3afe38646441cd80da37
Vitaliy Kosterov <kosterovvitaliy@gmail.com>
2022-08-05 12:24:11 +0300

    components

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,12 @@
+import styles from "./Card.module.scss";
 
 function Card(props) {
+	const onClickButton = () => {
+
+	}
+
 	return (
-		<div className="card">
+		<div className={styles.card}>
 			<div className="favorite">
 				<img src="/img/heart-unliked.svg" alt="Unliked" />
 			</div>
@@ -12,7 +17,7 @@ function Card(props) {
 					<span>Цена:</span>
 					<b>{props.price} $</b>
 				</div>
-				<button className="button">
+				<button className="button" onClick={onClickButton}>
 					<img width={11} height={11} src="/img/plus.svg" alt="Plus" />
 				</button>
 			</div>
